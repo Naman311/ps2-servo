e2659872fec152190ab0866bfc0639618787380f
Naman Gupta <namgupta96@gmail.com>
2017-08-17 20:07:48 +0530

    added 4 combination

--- thruster_ps2.ino
+++ thruster_ps2.ino
@@ -1,3 +1,19 @@
+/*R1 4096
+ *R2 16384
+ *L1 8192
+ *L2 32768
+ *TRIANGLE 2048 
+ *CIRCLE 1024 
+ *CROSS 512
+ *SQUARE 256
+ *START 16
+ *SELECT 128
+ *UP ARROW 8
+ *RIGHT ARROW 4
+ *DOWN ARROW 2
+ *LEFT ARROW 1
+ */
+
 #include <Servo.h>//Using servo library to control ESC
 Servo esc1;
 Servo esc2;
@@ -51,30 +67,46 @@ void loop() {
   data = Psx.read();                                      // Psx.read() initiates the PSX controller and returns
   Serial.println(data); 
   switch (data){
-    case 2048:            
-      esc1.writeMicroseconds();
+    case 2048:                  // first 8 seperate
+      esc1.writeMicroseconds(0);
       break;
     case 18432:
-      esc1.writeMicroseconds();
+      esc1.writeMicroseconds(0);
       break;
     case 1024:
-      esc2.writeMicroseconds();
+      esc2.writeMicroseconds(0);
       break;
     case 17408:            
-      esc2.writeMicroseconds();
+      esc2.writeMicroseconds(0);
       break;
     case 512:
-      esc3.writeMicroseconds();
+      esc3.writeMicroseconds(0);
       break;
     case 16896:
-      esc3.writeMicroseconds(1130);
+      esc3.writeMicroseconds(0);
       break;
     case 256:            
-      esc4.writeMicroseconds();
+      esc4.writeMicroseconds(0);
       break;
     case 16640:
-      esc4.writeMicroseconds();
+      esc4.writeMicroseconds(0);
       break;
+    case 4096:            //last 4 combination
+      esc1.writeMicroseconds(0);
+      esc2.writeMicroseconds(0);
+      break;
+    case 20480:
+      esc1.writeMicroseconds(0);
+      esc2.writeMicroseconds(0);
+      break;
+    case 8192:
+      esc3.writeMicroseconds(0);
+      esc4.writeMicroseconds(0);
+      break;
+    case 24576:
+      esc3.writeMicroseconds(0);
+      esc4.writeMicroseconds(0);
+      break;    
     case 0:
       esc1.writeMicroseconds(0);
       esc2.writeMicroseconds(0);
